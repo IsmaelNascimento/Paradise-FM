94f731da98b73e7c78a185067132507eaec3e1c0
Ismael Nascimento <contato@ismaelnascimento.com>
2018-01-27 19:46:21 -0200

    [UPDATE] Atualizado estruturas de pastas e mais.

---
+++ GGJCuritiba2018/Assets/_Project/Scripts/ScreenMainUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenMainUI : MonoBehaviour
+{
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void OnButtonExitGameClicked()
+    {
+        Application.Quit();
+    }
+}
