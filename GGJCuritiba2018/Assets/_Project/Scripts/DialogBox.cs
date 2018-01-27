81a7976a9e7293997e93b2bf9537317b06b0e9f5
Ismael Nascimento <contato@ismaelnascimento.com>
2018-01-27 10:34:47 -0200

    [UPDATE] Invetario de simbolos

--- GGJCuritiba2018/Assets/_Project/Scripts/DialogBox.cs
+++ GGJCuritiba2018/Assets/_Project/Scripts/DialogBox.cs
@@ -1,7 +1,46 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Events;
 
 public class DialogBox : MonoBehaviour
 {
-    public TextMeshProUGUI textDialogBox;
+    private static DialogBox m_Instance;
+    public static DialogBox Instance
+    {
+        get
+        {
+            if (m_Instance == null)
+                m_Instance = FindObjectOfType(typeof(DialogBox)) as DialogBox;
+
+            return m_Instance;
+        }
+    }
+
+    [SerializeField] private TextMeshProUGUI textDialogBox;
+    [SerializeField] private Button m_ButtonDialogBox;
+
+    [ContextMenu("SetTextDialogBox")]
+    public void SetTextDialogBox(string newText)
+    {
+        textDialogBox.text = newText;
+    }
+
+    [ContextMenu("AnimationInput")]
+    public void AnimationInput()
+    {
+        GetComponent<Animator>().Play("DialogBoxInput");
+    }
+
+    [ContextMenu("AnimationOutput")]
+    public void AnimationOutput()
+    {
+        GetComponent<Animator>().Play("DialogBoxOutput");
+    }
+
+    public void ActionButtonDialofgBox(UnityAction action)
+    {
+        if (action != null)
+            m_ButtonDialogBox.onClick.AddListener(action);
+    }
 }
---
+++ GGJCuritiba2018/Assets/_Project/Scripts/SymbolsInventory.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SymbolsInventory : MonoBehaviour
+{
+    [ContextMenu("OpenSymbolsInvetory")]
+    public void OpenSymbolsInvetory()
+    {
+        GetComponent<Animator>().Play("SymbolsInvetoryInput");
+    }
+
+    [ContextMenu("CloseSymbolsInvetory")]
+    public void CloseSymbolsInvetory()
+    {
+        GetComponent<Animator>().Play("SymbolsInvetoryOutput");
+    }
+}
