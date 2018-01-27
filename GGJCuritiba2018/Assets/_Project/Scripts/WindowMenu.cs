893c9f4f5c3593927d564bc52188026c9ea360b8
Ismael Nascimento <contato@ismaelnascimento.com>
2018-01-27 13:17:34 -0200

    [ADD] Prefab da janela de opções do menu no gameplay.

---
+++ GGJCuritiba2018/Assets/_Project/Scripts/WindowMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WindowMenu : MonoBehaviour
+{
+    public void OnButtonMenuClicked(GameObject menu)
+    {
+        menu.SetActive(true);
+    }
+
+    public void OnButtonContinueClicked(GameObject windowMenu)
+    {
+        windowMenu.SetActive(false);
+    }
+
+    public void OnButtonSetupClicked()
+    {
+
+    }
+
+    public void OnButtonHomeClicked(string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+}
