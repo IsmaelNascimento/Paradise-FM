f27e33c816ee5f64254ccb3bf01d44f37a70b425
Vítor Fernandes <vitor_resende@ymail.com>
2018-01-27 14:52:21 -0200

    Created WaitStep

---
+++ GGJCuritiba2018/Assets/_Project/Scripts/Steps/WaitStep.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitStep : BaseStep
+{
+    public float SecondsToWait;
+
+    private void OnEnable()
+    {
+        StartCoroutine(WaitAndGo());
+    }
+
+    private IEnumerator WaitAndGo()
+    {
+        yield return new WaitForSeconds(SecondsToWait);
+        EndStep();
+    }
+}
