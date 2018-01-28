a320c6b6b87dcad12a9f43ea3eb9075fd6b194e3
Vítor Fernandes <vitor_resende@ymail.com>
2018-01-27 22:32:31 -0200

    Added a Fade In step for sprites

---
+++ GGJCuritiba2018/Assets/_Project/Scripts/Steps/FadeInStep.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInStep : BaseStep
+{
+    public SpriteRenderer[] SpritesToFadeIn;
+    public float FadeInTime;
+
+    public void OnEnable()
+    {
+        for (int spriteIndex = 0; spriteIndex < SpritesToFadeIn.Length; spriteIndex++)
+        {
+            SpritesToFadeIn[spriteIndex].DOFade(1f, FadeInTime);
+        }
+        StartCoroutine(WaitAndGo());
+    }
+
+    private IEnumerator WaitAndGo()
+    {
+        yield return new WaitForSeconds(FadeInTime);
+        EndStep();
+    }
+}
