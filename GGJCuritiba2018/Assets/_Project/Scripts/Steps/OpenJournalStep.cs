1ada5e2b44ea71f7c70749e515a85c6a95efe4e8
Vítor Fernandes <vitor_resende@ymail.com>
2018-01-28 15:04:20 -0200

    Full Game

---
+++ GGJCuritiba2018/Assets/_Project/Scripts/Steps/OpenJournalStep.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenJournalStep : BaseStep
+{
+    private void OnEnable()
+    {
+        HudManager.Instance.OpenOrCloseBook();
+        EndStep();
+    }
+}
