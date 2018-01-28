8719445c2b380a23cf9973ca6cc629bf7cce976a
Vítor Fernandes <vitor_resende@ymail.com>
2018-01-28 15:26:24 -0200

    Acabou

---
+++ GGJCuritiba2018/Assets/_Project/Scripts/Steps/StartSoundStep.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSoundStep : BaseStep
+{
+    public AudioSource source;
+    public AudioClip clip;
+
+    private void OnEnable()
+    {
+        source.clip = clip;
+        source.volume = 1f;
+        source.Play();
+        EndStep();
+    }
+}
---
+++ GGJCuritiba2018/Assets/_Project/Scripts/Steps/StopSoundStep.cs
@@ -0,0 +1,18 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopSoundStep : BaseStep
+{
+    public AudioSource source;
+
+    private void OnEnable()
+    {
+        source.DOFade(0f, 1f).OnComplete(() =>
+        {
+            EndStep();
+        });
+    }
+
+}
