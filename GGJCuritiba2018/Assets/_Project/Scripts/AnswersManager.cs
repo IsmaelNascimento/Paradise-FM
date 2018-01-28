597180a039e0b59990f91716a337b4d49f351eea
Ismael Nascimento <contato@ismaelnascimento.com>
2018-01-28 08:38:46 -0200

    [UPDATE] Adicionado Hud para Gameplay.

---
+++ GGJCuritiba2018/Assets/_Project/Scripts/AnswersManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswersManager : MonoBehaviour
+{
+    private static AnswersManager m_Instance;
+    public static AnswersManager Instance
+    {
+        get
+        {
+            if (m_Instance == null)
+                m_Instance = FindObjectOfType(typeof(AnswersManager)) as AnswersManager;
+
+            return m_Instance;
+        }
+    }
+
+    [SerializeField] private GameObject[] m_Buttons;
+
+    private void Start()
+    {
+        for (int i = 0; i < m_Buttons.Length; i++)
+            m_Buttons[i].SetActive(false);
+    }
+
+    public void ActivePanelAnswers(int countAnswers)
+    {
+        for (int i = 0; i < countAnswers; i++)
+            m_Buttons[i].SetActive(true);
+    }
+
+    public void OnButtonReturnIDClicked(int id)
+    {
+        GetIDSelected(id);
+    }
+
+    public int GetIDSelected(int id)
+    {
+        return id;
+    }
+
+    public void SetTextButtonAnswers(int idButton, string textAnswers)
+    {
+        switch (idButton)
+        {
+            case 0:
+                m_Buttons[0].GetComponentInChildren<Text>().text = textAnswers;
+                break;
+            case 1:
+                m_Buttons[1].GetComponentInChildren<Text>().text = textAnswers;
+                break;
+            case 2:
+                m_Buttons[2].GetComponentInChildren<Text>().text = textAnswers;
+                break;
+        }
+    }
+
+    #region Tests com ContextMenu
+
+    [ContextMenu("TestSetTextButtonAnswers")]
+    public void TestSetTextButtonAnswers()
+    {
+        SetTextButtonAnswers(1, "Minha resposta em ingles");
+    }
+
+    [ContextMenu("TestActivePanelAnswers")]
+    public void TestActivePanelAnswers()
+    {
+        ActivePanelAnswers(3);
+    }
+
+    #endregion
+}
---
+++ GGJCuritiba2018/Assets/_Project/Scripts/HUD/HudManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudManager : MonoBehaviour
+{
+    private static HudManager m_Instance;
+    public static HudManager Instance
+    {
+        get
+        {
+            if (m_Instance == null)
+                m_Instance = FindObjectOfType(typeof(HudManager)) as HudManager;
+
+            return m_Instance;
+        }
+    }
+
+    [SerializeField] private Text m_TextClock;
+    private bool m_IsOpenBook = false;
+
+    public void SetNewHourInClock(string hour)
+    {
+        m_TextClock.text = hour;
+    }
+
+    #region Buttons
+
+    public void OpenBook()
+    {
+        print("Book open");
+    }
+
+    public void ChangeVolumeMusic(Slider slider)
+    {
+        print("slider volume value:: " + slider.value);
+    }
+
+    public void ChangeVolumeFx(Slider slider)
+    {
+        print("slider fx value:: " + slider.value);
+    }
+
+    #endregion
+}
