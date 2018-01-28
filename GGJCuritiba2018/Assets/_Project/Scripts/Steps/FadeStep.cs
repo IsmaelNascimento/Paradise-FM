833baba836fbe0189afc08ea1fe937faaf9780bd
Vítor Fernandes <vitor_resende@ymail.com>
2018-01-27 23:33:41 -0200

    Melhorias de roteiro

---
+++ GGJCuritiba2018/Assets/_Project/Scripts/Steps/FadeStep.cs
@@ -0,0 +1,26 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeStep : BaseStep
+{
+    public SpriteRenderer[] SpritesToFade;
+    public float FadeTo;
+    public float FadeTime;
+
+    public void OnEnable()
+    {
+        for (int spriteIndex = 0; spriteIndex < SpritesToFade.Length; spriteIndex++)
+        {
+            SpritesToFade[spriteIndex].DOFade(FadeTo, FadeTime);
+        }
+        StartCoroutine(WaitAndGo());
+    }
+
+    private IEnumerator WaitAndGo()
+    {
+        yield return new WaitForSeconds(FadeTime);
+        EndStep();
+    }
+}
