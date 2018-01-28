503f10d4ead92358d29200892625b20821ddc9b0
Vítor Fernandes <vitor_resende@ymail.com>
2018-01-28 05:42:45 -0200

    Story all the way to opening the door

---
+++ GGJCuritiba2018/Assets/_Project/Scripts/Steps/SoundStep.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundStep : BaseStep
+{
+    public AudioClip Sound;
+
+    private void OnEnable()
+    {
+        StepController.Instance.StepAudioSource.clip = Sound;
+        StepController.Instance.StepAudioSource.Play();
+        EndStep();
+    }
+}
