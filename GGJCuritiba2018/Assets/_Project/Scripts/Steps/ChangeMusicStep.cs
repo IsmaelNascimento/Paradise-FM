3c23c16e969cf89dc55a7ec8d329b4ac71fcdbf5
Vítor Fernandes <vitor_resende@ymail.com>
2018-01-28 10:18:20 -0200

    Almost everything in

---
+++ GGJCuritiba2018/Assets/_Project/Scripts/Steps/ChangeMusicStep.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMusicStep : BaseStep
+{
+    public AudioSource Source;
+    public AudioClip Clip;
+
+    private void OnEnable()
+    {
+        Source.clip = Clip;
+        Source.DOFade(0f, 1f).OnComplete(() =>
+        {
+            Source.clip = Clip;
+            Source.Play();
+            Source.DOFade(1f, 1f).OnComplete(() =>
+            {
+                EndStep();
+            }); ;
+        });
+
+    }
+}
