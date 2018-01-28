8ee541d889b127b97407127ec9b7e651282077be
Ismael Nascimento <contato@ismaelnascimento.com>
2018-01-28 02:16:21 -0200

    [UPDATE] Pequenas alterações

--- GGJCuritiba2018/Assets/_Project/Scripts/Steps/TextStep.cs
+++ GGJCuritiba2018/Assets/_Project/Scripts/Steps/TextStep.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class TextStep : BaseStep
 {
     public string[] Text;
+    [Space(15)]
     public AudioClip[] songs;
     private int textIndex;
     private int charIndex;
